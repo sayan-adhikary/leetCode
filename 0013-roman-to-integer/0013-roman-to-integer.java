6e1685461a107bc7ca9f8c2c96cd21e58d8ec66f
SAYAN ADHIKARY <103528635+sayan-adhikary@users.noreply.github.com>
2024-12-24 23:22:37 +0530

    Time: 3 ms (86.66%), Space: 44.8 MB (53.93%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,24 +1,19 @@
 class Solution {
-    private static int calcScale(char c, char a1, char a2) {
-        return (c == a1 || c == a2) ? -1 : 1;
-    }
-    
     public int romanToInt(String s) {
-        int result = 0;
-        
-        for (int n = 0; n < s.length(); n++) {
-            char nextChar = (n + 1 < s.length()) ? s.charAt(n + 1) : '\0';
-            
-            switch (s.charAt(n)) {
-                case 'M': result += 1000; break;
-                case 'D': result += 500; break;
-                case 'C': result += 100 * calcScale(nextChar, 'M', 'D'); break;
-                case 'L': result += 50; break;
-                case 'X': result += 10 * calcScale(nextChar, 'C', 'L'); break;
-                case 'V': result += 5; break;
-                case 'I': result += 1 * calcScale(nextChar, 'X', 'V'); break;
+        int value=0,prev=0;
+        for(char ch: s.toCharArray())
+        {
+            switch(ch)
+            {
+                case 'I': value+=1;prev=1;break;
+                case 'V': value+=(prev==1)?3:5;prev=5;break;
+                case 'X': value+=(prev==1)?8:10;prev=10;break;
+                case 'L': value+=(prev==10)?30:50;prev=50;break;
+                case 'C': value+=(prev==10)?80:100;prev=100;break;
+                case 'D': value+=(prev==100)?300:500;prev=500;break;
+                case 'M': value+=(prev==100)?800:1000;prev=1000;break;
             }
         }
-        return result;
+        return value;
     }
 }
