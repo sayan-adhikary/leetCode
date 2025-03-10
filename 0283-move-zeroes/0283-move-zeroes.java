302e4e041c59da9447c722fbc7e0167d06b32a93
SAYAN ADHIKARY <103528635+sayan-adhikary@users.noreply.github.com>
2025-03-11 02:10:16 +0530

    Time: 142 ms (5.01%), Space: 45.7 MB (90.55%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -9,5 +9,8 @@ class Solution {
         for (int i = index; i < nums.length; i++) {
             nums[i] = 0;
         }
+        for(int i=0;i<nums.length;i++){
+             System.out.print(nums[i]+" ");
+        }
     }
 }
