58d2bf652c5a6c2aa8721e0dba3579682bb107ec
SAYAN ADHIKARY <103528635+sayan-adhikary@users.noreply.github.com>
2025-09-11 09:59:13 +0530

    Time: 2 ms (99.99%), Space: 43.7 MB (15.79%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,27 +1,59 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        Arrays.sort(nums);
-        int len = nums.length;
-        int prevDif= Integer.MAX_VALUE;
-        int ans = 0;
-        for(int i = 0;i< len;i++ ){
-            int j = i+1;
-            int k = len-1;
-            while(j < k){
-                int sum = nums[i] + nums[j] + nums[k];
-                if(Math.abs(sum-target) < prevDif){
-                    prevDif = Math.abs(sum-target);
-                    ans = sum;
-                }
+         byte[] count = new byte[2001];
+        for (int i = 0; i < nums.length; i++) {
+            count[nums[i]+1000]++;
+        }
 
-                if(sum > target){
-                    k--;
-                }
-                else{
-                    j++;
-                }
+        for(int k=0,i=0; k< count.length;k++){
+            while (count[k] > 0){
+                nums[i] = k - 1000;
+                i++;
+                count[k]--;
             }
         }
-        return ans;
+
+        int low = 0, high = nums.length-1;
+        while (low+1 < high -1 &&nums[low+1]+nums[high]+nums[high-1]< target){
+            low ++;
+        }
+
+        while ( high-1> low +1&& nums[low]+nums[low+1]+nums[high-1] > target){
+            high --;
+        }
+
+        int closet = Integer.MAX_VALUE;
+        int closestresult = 0;
+        for(int i = low; i < high-1; i++) {
+
+            int k = i+1;
+            int j = high;
+            while(k < j) {
+                int total = nums[i] + nums[k] + nums[j];
+                if (total > target) {
+                    j--;
+                    while (j>k&&nums[j]==nums[j+1]){
+                        j--;
+                    }
+                } else if (total < target) {
+                    k++;
+                    while (k<j&&nums[k]==nums[k-1]){
+                        k++;
+                    }
+                } else {
+                    return target;
+                }
+
+                int minus = Math.abs(total - target);
+                if (minus < closet) {
+                    closet = minus;
+                    closestresult = total;
+                }
+
+            }
+            while (i+1 < high-1&&nums[i] == nums[i+1]) i++;
+        }
+
+        return closestresult;
     }
 }
