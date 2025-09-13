8b6df9cde66dde7e14d883bc43762628463ac32c
SAYAN ADHIKARY <103528635+sayan-adhikary@users.noreply.github.com>
2025-09-13 09:44:27 +0530

    Time: 330 ms (99.76%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,11 +1,5 @@
-SELECT 
-    w1.id
-FROM 
-    Weather w1
-JOIN 
-    Weather w2
-
-ON 
-    DATEDIFF(w1.recordDate, w2.recordDate) = 1
-WHERE 
-    w1.temperature > w2.temperature;
+SELECT w1.id
+FROM Weather AS w1
+JOIN Weather AS w2
+  ON w2.recordDate = DATE_SUB(w1.recordDate, INTERVAL 1 DAY)
+WHERE w1.temperature > w2.temperature;
