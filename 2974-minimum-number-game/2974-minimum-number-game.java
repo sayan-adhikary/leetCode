62d3b8db57d77b7f5b1226593541575277c57450
SAYAN ADHIKARY <103528635+sayan-adhikary@users.noreply.github.com>
2026-02-16 15:38:01 +0530

    Time: 5 ms (79.89%), Space: 47.1 MB (61.12%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.java
+++ 2974-minimum-number-game/2974-minimum-number-game.java
@@ -1,15 +1,14 @@
 class Solution {
     public int[] numberGame(int[] nums) {
+        //[5,4,2,3] => [2,3,4,5] ==> [3,2,5,4]
+        int[] arr = new int[nums.length];
         Arrays.sort(nums);
-        int left = 0;
-        int right = 1;
-        while (right < nums.length) {
-            int temp = nums[left];
-            nums[left] = nums[right];
-            nums[right] = temp;
-            left += 2;
-            right += 2;
+        int i = 0;
+        while(i < nums.length - 1){
+            arr[i] = nums[i+1];
+            arr[i + 1] = nums[i];
+            i +=2;
         }
-        return nums;
+        return arr;
     }
 }
