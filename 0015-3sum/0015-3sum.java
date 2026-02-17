339ca675dd26f4c4369f767a70eac0402b791ee5
SAYAN ADHIKARY <103528635+sayan-adhikary@users.noreply.github.com>
2026-02-17 18:35:37 +0530

    Time: 1606 ms (5%), Space: 56 MB (96.34%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,45 +1,45 @@
 class Solution{
-// Function to find triplets having sum equals to 0
-public List<List<Integer>> threeSum(int[] nums) {
-    // Set to store unique triplets
-    Set<List<Integer>> tripletSet = new HashSet<>();
+    // Function to find triplets having sum equals to 0
+    public List<List<Integer>> threeSum(int[] nums) {
+        // Set to store unique triplets
+        Set<List<Integer>> tripletSet = new HashSet<>();
 
-    int n = nums.length;
+        int n = nums.length;
 
-    // Check all possible triplets
-    for (int i = 0; i < n; i++) {
-        // Set to store elements seen so far in the loop
-        Set<Integer> hashset = new HashSet<>();
+        // Check all possible triplets
+        for (int i = 0; i < n; i++) {
+            // Set to store elements seen so far in the loop
+            Set<Integer> hashset = new HashSet<>();
 
-        for (int j = i + 1; j < n; j++) {
-            // Calculate the 3rd element needed to reach 0
-            int third =  - (nums[i] + nums[j]);
+            for (int j = i + 1; j < n; j++) {
+                // Calculate the 3rd element needed to reach 0
+                int third =  - (nums[i] + nums[j]);
 
-            /* Find if third element exists in
-            hashset (complements seen so far)*/
-            if (hashset.contains(third)) {
-                // Found a triplet that sums up to target
-                List<Integer> temp = new ArrayList<>();
-                temp.add(nums[i]);
-                temp.add(nums[j]);
-                temp.add(third);
+                /* Find if third element exists in
+                hashset (complements seen so far)*/
+                if (hashset.contains(third)) {
+                    // Found a triplet that sums up to target
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(third);
 
-                /* Sort the triplet to ensure
-                uniqueness when storing in set*/
-                Collections.sort(temp);
-                tripletSet.add(temp);
+                    /* Sort the triplet to ensure
+                    uniqueness when storing in set*/
+                    Collections.sort(temp);
+                    tripletSet.add(temp);
+                }
+
+                /* Insert the current element 
+                into hashset for future checks*/
+                hashset.add(nums[j]);
             }
-
-            /* Insert the current element 
-             into hashset for future checks*/
-            hashset.add(nums[j]);
         }
+
+        // Convert set to list of lists (unique triplets)
+        List<List<Integer>> ans = new ArrayList<>(tripletSet);
+
+        //Return the ans
+        return ans;
     }
-
-    // Convert set to list of lists (unique triplets)
-    List<List<Integer>> ans = new ArrayList<>(tripletSet);
-
-    //Return the ans
-    return ans;
-}
 }
