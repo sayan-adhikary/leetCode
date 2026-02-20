30ff959a798948bc95d6a267a96d233d7e565d40
SAYAN ADHIKARY <103528635+sayan-adhikary@users.noreply.github.com>
2026-02-20 17:34:12 +0530

    Time: 0 ms (100%), Space: 53.7 MB (73.55%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,5 +3,22 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    nums.sort((a,b) => a - b);
+    let low = 0, mid = 0, high = nums.length -  1;
+
+    while(mid <= high){
+        if(nums[mid] === 0){
+            let temp = nums[low];
+            nums[low] = nums[mid];
+            nums[mid] = temp;
+            low++;
+            mid++;
+        } else if(nums[mid] === 1){
+            mid++;
+        } else if(nums[mid] === 2) {
+            let temp = nums[mid];
+            nums[mid] = nums[high];
+            nums[high] = temp;
+            high--;
+        }
+    }
 };
