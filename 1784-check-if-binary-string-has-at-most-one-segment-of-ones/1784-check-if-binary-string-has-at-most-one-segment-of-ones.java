cc312d914ce4f40b3faee44c2ff020f9623cf69a
SAYAN ADHIKARY <103528635+sayan-adhikary@users.noreply.github.com>
2026-03-07 00:11:40 +0530

    Time: 0 ms (100%), Space: 43 MB (50.94%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.java
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.java
@@ -1,5 +1,16 @@
 class Solution {
     public boolean checkOnesSegment(String s) {
-        return !s.contains("01");
+        int point = 0;
+        if(s.indexOf('0') == -1){
+            return true;
+        } else{
+            point = s.indexOf('0');
+        }
+        for(int i = point; i < s.length(); i++){
+            if(s.charAt(i) == '1'){
+                return false;
+            }
+        }
+        return true;
     }
 }
