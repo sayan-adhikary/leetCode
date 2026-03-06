2c5a4b92d54860b87d2c7be5c59985db3c482018
SAYAN ADHIKARY <103528635+sayan-adhikary@users.noreply.github.com>
2026-03-06 10:26:47 +0530

    Time: 5 ms (54.02%), Space: 43.3 MB (99.49%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -3,11 +3,10 @@ class Solution {
         int count1 = 0, count2 = 0;
 
         for(int i = 0; i < s.length(); i++){
-            char str1 = (i % 2 == 0) ? '0' : '1';
-            char str2 = (i % 2 == 0) ? '1' : '0';
+           char ch = s.charAt(i);
 
-            if(s.charAt(i) != str1) count1++;
-            if(s.charAt(i) != str2) count2++;
+           if(ch != (i % 2 == 0 ? '1' : '0')) count1++;
+           if(ch != (i % 2 == 0 ? '0' : '1')) count2++;
         }
         return Math.min(count1, count2);
     }
