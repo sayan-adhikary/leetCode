e13da6df93c423d1924f5d73b5caf459a9a716b5
SAYAN ADHIKARY <103528635+sayan-adhikary@users.noreply.github.com>
2026-03-08 08:11:30 +0530

    Time: 28 ms (35.92%), Space: 47.3 MB (59.18%) - LeetHub

--- 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.java
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.java
@@ -1,17 +1,47 @@
 class Solution {
     public int minFlips(String s) {
         int n = s.length();
-        int res = n;
-        int[] op = { 0, 0 };
+        String extended = s + s;
 
-        for (int i = 0; i < n; i++)
-            op[(s.charAt(i) ^ i) & 1]++;
+        int mismatch1 = 0; // for pattern "010101..."
+        int mismatch2 = 0; // for pattern "101010..."
+        int left = 0;
+        int result = Integer.MAX_VALUE;
 
-        for (int i = 0; i < n; i++) {
-            op[(s.charAt(i) ^ i) & 1]--;
-            op[(s.charAt(i) ^ (n + i)) & 1]++;
-            res = Math.min(res, Math.min(op[0], op[1]));
+        for (int right = 0; right < extended.length(); right++) {
+
+            char expected1 = (right % 2 == 0) ? '0' : '1';
+            char expected2 = (right % 2 == 0) ? '1' : '0';
+
+            if (extended.charAt(right) != expected1) {
+                mismatch1++;
+            }
+
+            if (extended.charAt(right) != expected2) {
+                mismatch2++;
+            }
+
+            if (right - left + 1 > n) {
+
+                char leftExpected1 = (left % 2 == 0) ? '0' : '1';
+                char leftExpected2 = (left % 2 == 0) ? '1' : '0';
+
+                if (extended.charAt(left) != leftExpected1) {
+                    mismatch1--;
+                }
+
+                if (extended.charAt(left) != leftExpected2) {
+                    mismatch2--;
+                }
+
+                left++;
+            }
+
+            if (right - left + 1 == n) {
+                result = Math.min(result, Math.min(mismatch1, mismatch2));
+            }
         }
-        return res;
+
+        return result;
     }
 }
