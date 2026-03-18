191cc60e82881078e47345140305c40d042f2b60
SAYAN ADHIKARY <103528635+sayan-adhikary@users.noreply.github.com>
2026-03-18 11:16:09 +0530

    Time: 1 ms (95.14%), Space: 43.3 MB (82.78%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,27 +1,25 @@
 class Solution {
+
+    //HELPER
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> list = new ArrayList<>();
+
+        list.add(1);
+
+        for (int i = 1; i < rowIndex; i++) {
+            long val = (long) list.get(i - 1) * (rowIndex - i) / i;
+            list.add((int) val);
+        }
+
+        return list;
+    }
+
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> pascalTriangle = new ArrayList<>();
 
-        for (int row = 1; row <= numRows; row++) {
-            pascalTriangle.add(generateRow(row));
+        for (int r = 1; r <= numRows; r++) {
+            pascalTriangle.add(getRow(r));
         }
         return pascalTriangle;
-
-    }
-
-    //HELPER
-    private List<Integer> generateRow(int row) {
-        long ans = 1;
-        List<Integer> ansRow = new ArrayList<>();
-
-        ansRow.add(1);
-
-        for (int col = 1; col < row; col++) {
-            ans = ans * (row - col);
-            ans = ans / col;
-            ansRow.add((int) ans);
-        }
-
-        return ansRow;
     }
 }
