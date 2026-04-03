0a129b5c749b32ba0d99bd07f805514d2b350222
SAYAN ADHIKARY <103528635+sayan-adhikary@users.noreply.github.com>
2026-04-03 23:39:53 +0530

    Time: 69 ms (5.02%), Space: 60.4 MB (5.08%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    let st = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+            st.push(s[i]);
+        } else {
+            if (st.length > 0) {
+                let ch = st[st.length - 1];
+                // {
+                ch += s[i];
+                // ch = {}
+                st.pop();
+                console.log(ch);
+                if (ch == "()" || ch == "{}" || ch == "[]") {
+                    continue;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+    if (st.length > 0) {
+        return false;
+    }
+    return true;
+};
