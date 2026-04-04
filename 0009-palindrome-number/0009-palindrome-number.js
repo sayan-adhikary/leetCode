3e33d63db99fd6e22ce3c1ac92e4551e5b24b96b
SAYAN ADHIKARY <103528635+sayan-adhikary@users.noreply.github.com>
2026-04-04 22:12:23 +0530

    Time: 67 ms (5.09%), Space: 65.8 MB (6.82%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -6,5 +6,5 @@ var isPalindrome = function(x) {
     if(x < 0) return false;
     let str = x.toString().split("").reverse().join("");
     console.log(str);
-    return (x == str) ? true : false;
+    return (x == str);
 };
