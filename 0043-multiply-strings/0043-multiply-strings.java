31872cfb06b62ca93e156c7016355310741c032e
SAYAN ADHIKARY <103528635+sayan-adhikary@users.noreply.github.com>
2026-04-17 09:10:42 +0530

    Time: 2 ms (99.82%), Space: 43.4 MB (85.62%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        int n = num1.length();
+        int m = num2.length();
+
+        int[] result = new int[n + m];
+
+        for (int i = n - 1; i >= 0; i--) {
+            int d1 = num1.charAt(i) - '0';
+
+            for (int j = m - 1; j >= 0; j--) {
+                int d2 = num2.charAt(j) - '0';
+
+                int sum = d1 * d2 + result[i + j + 1];
+
+                result[i + j + 1] = sum % 10;
+                result[i + j] += sum / 10;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        int i = 0;
+        while (i < result.length && result[i] == 0)
+            i++;
+
+        for (; i < result.length; i++) {
+            sb.append(result[i]);
+        }
+
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}
