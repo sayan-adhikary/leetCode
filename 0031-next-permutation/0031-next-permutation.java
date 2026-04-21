e820f412df864c81ab686cff185c323ff4001560
sayan89 <103528635+sayan-adhikary@users.noreply.github.com>
2026-04-21 13:32:49 +0530

    Time: 0 ms (100%), Space: 44.8 MB (17.98%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,43 @@
+class Solution {
+    private void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public void nextPermutation(int[] nums) {
+
+        if (nums == null || nums.length <= 1)
+            return;
+
+        int n = nums.length;
+        int idx = -1;
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                idx = i;
+                break;
+            }
+        }
+
+        if (idx == -1) {
+            reverse(nums, 0, n - 1);
+            return;
+        }
+
+        for (int i = n - 1; i > idx; i--) {
+            if (nums[i] > nums[idx]) {
+                int temp = nums[i];
+                nums[i] = nums[idx];
+                nums[idx] = temp;
+                break;
+            }
+        }
+
+        reverse(nums, idx + 1, n - 1);
+    }
+}
