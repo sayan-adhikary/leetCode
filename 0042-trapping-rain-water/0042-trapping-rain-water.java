d6498470c9b4ea9efea32067eb00344dbdfb2723
sayan89 <103528635+sayan-adhikary@users.noreply.github.com>
2026-04-23 16:35:36 +0530

    Time: 7 ms (4.49%), Space: 48.6 MB (29.85%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,18 +1,30 @@
 class Solution {
     public int trap(int[] height) {
         int n = height.length;
-        //calculate left max boundary - array
-        int leftmax[] = new int[n];
+
+        int[] leftmax = new int[n];
+        int[] rightmax = new int[n];
+
+        //calculate left max boundary - stack
+        Stack<Integer> leftmaxSt = new Stack<>();
+        leftmaxSt.push(height[0]);
         leftmax[0] = height[0];
         for (int i = 1; i < n; i++) {
-            leftmax[i] = Math.max(height[i], leftmax[i - 1]);
+            if(height[i] > leftmaxSt.peek()){
+                leftmaxSt.push(height[i]);
+            }
+            leftmax[i] = leftmaxSt.peek();
         }
 
-        //calculate right max boundary - array
-        int rightmax[] = new int[n];
+        //calculate right max boundary - Stack
+        Stack<Integer> rightmaxSt = new Stack<>();
+        rightmaxSt.push(height[n - 1]);
         rightmax[n - 1] = height[n - 1];
         for (int i = n - 2; i >= 0; i--) {
-            rightmax[i] = Math.max(height[i], rightmax[i + 1]);
+            if(height[i] > rightmaxSt.peek()){
+                rightmaxSt.push(height[i]);
+            }
+            rightmax[i] = rightmaxSt.peek();
         }
 
         //loop
