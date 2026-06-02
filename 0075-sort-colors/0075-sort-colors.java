817a4e1b2867d26143d9773f4e0cc0e736d918c8
Sayan Adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-06-02 18:24:21 +0530

    Time: 0 ms (100%), Space: 43.7 MB (23.15%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,21 +1,30 @@
 class Solution {
+    /*
+     * Approach :- (Optimized) [Dutch National flag algo]
+     * 1. one's position should check until it will not run out the two's poisition;
+     * 2. check if one's elem is 0 then swap with zero'th elem and zero++ & one++;
+     * 3. else if one's elem is 1 then just one++;
+     * 4. else if one's elem is 2 then swap with two's elem only two--;
+     */
     public void sortColors(int[] nums) {
-        int low = 0, mid = 0, high = nums.length - 1;
+        
         //[0 0 0 0 | 1 1 1 1 | 2 2 2 2] => [low | mid | high] 
-        while(mid <= high){
-            if(nums[mid] == 0){
-                int temp = nums[low];
-                nums[low] = nums[mid];
-                nums[mid] = temp;
-                low++;
-                mid++;
-            } else if(nums[mid] == 1){
-                mid++;
-            } else if(nums[mid] == 2){
-                int temp = nums[mid];
-                nums[mid] = nums[high];
-                nums[high] = temp;
-                high--;
+        
+        int zero = 0, one = 0, two = nums.length - 1;
+        while (one <= two) {
+            if (nums[one] == 0) {
+                int temp = nums[zero];
+                nums[zero] = nums[one];
+                nums[one] = temp;
+                zero++;
+                one++;
+            } else if (nums[one] == 1) {
+                one++;
+            } else {
+                int temp = nums[two];
+                nums[two] = nums[one];
+                nums[one] = temp;
+                two--;
             }
         }
     }
