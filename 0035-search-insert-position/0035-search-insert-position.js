9eb763954b344ae808f29794f73c680911477d42
Sayan Adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-08-27 23:59:46 +0530

    Time: 0 ms (100%), Space: 53.5 MB (68.58%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,10 +4,17 @@
  * @return {number}
  */
 var searchInsert = function (nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] >= target) {
-            return i;
+    let low = 0, high = nums.length - 1;
+    let ans = nums.length;
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+
+        if (nums[mid] >= target) {
+            ans = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
         }
     }
-    return nums.length;
+    return ans;
 };
