cbb6f26979219bee5036fe58571dad04a01d4d2f
Sayan Adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-09-09 12:47:53 +0530

    Time: 3 ms (41.87%), Space: 54.8 MB (97.26%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -2,12 +2,17 @@
  * @param {number[]} nums
  * @return {number}
  */
-var missingNumber = function(nums) {
-    let n = nums.length;
-    let sumOfN = n * (n + 1) / 2;
-    let current = 0;
-    for(let i = 0; i < n; i++){
-        current += nums[i];
+var missingNumber = function (nums) {
+    let totalSum = 0;
+    for (let i = 1; i <= nums.length; i++) {
+        totalSum += i;
     }
-    return sumOfN - current;
+
+    let elementSum = 0
+    for (let i = 0; i < nums.length; i++) {
+        elementSum += nums[i];
+    }
+    return totalSum - elementSum;
 };
+
+//[9,6,4,2,3,5,7,0,1] -> 9
