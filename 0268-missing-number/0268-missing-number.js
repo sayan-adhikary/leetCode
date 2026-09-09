4cf999cd1cfaf7e02759d0b639249f22583a0b01
Sayan Adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-09-09 13:54:45 +0530

    Time: 1 ms (69.11%), Space: 57.4 MB (31.74%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,10 +3,10 @@
  * @return {number}
  */
 var missingNumber = function (nums) {
-    let totalSum = 0;
-    for (let i = 1; i <= nums.length; i++) {
-        totalSum += i;
-    }
+    let n=nums.length;
+
+    let totalSum = n*(n+1)/2
+    
 
     let elementSum = 0
     for (let i = 0; i < nums.length; i++) {
