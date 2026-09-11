3eb6bdd71878b0de9a5958f991101903e908afb7
Sayan Adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-09-11 19:24:18 +0530

    Time: 3 ms (77.94%), Space: 61.4 MB (81.81%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -2,19 +2,20 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortedSquares = function(nums) {
+var sortedSquares = function (nums) {
     let left = 0;
     let right = nums.length - 1;
-    let ans = [];
+    let last = nums.length - 1;
+    let ans = new Array(nums.length);
 
-    while(left <= right){
-        if(nums[left]**2 < nums[right]**2){
-            ans.push(nums[right]**2);
+    while (left <= right) {
+        if (nums[left] ** 2 < nums[right] ** 2) {
+            ans[last--] = nums[right] ** 2;
             right--;
         } else {
-            ans.push(nums[left]**2);
+            ans[last--] = nums[left] ** 2;
             left++;
         }
     }
-    return ans.reverse();
+    return ans;
 };
