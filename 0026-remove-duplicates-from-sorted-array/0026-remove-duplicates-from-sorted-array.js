a9bc24d4cc6c004b98fd622dff6e2c6847485eba
Sayan Adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-09-11 20:13:54 +0530

    Time: 0 ms (100%), Space: 58 MB (48.07%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -2,18 +2,12 @@
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function (arr) {
-    if(arr.length == 1){
-        return 1;
-    }
-    let left = 0;
-    let right = 1;
-    let n = arr.length;
-    while (right < n) {
-        while (right < n && arr[left] == arr[right]) {
-            right++;
+var removeDuplicates = function (nums) {
+    let j = 1;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] != nums[i - 1]) {
+            nums[j++] = nums[i]
         }
-        arr[++left] = arr[right];
     }
-    return left;
+    return j;
 };
