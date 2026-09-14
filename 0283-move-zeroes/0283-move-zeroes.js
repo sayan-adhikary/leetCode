8d0928e87b19652ab2694c51169eee7845e0219b
Sayan Adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-09-14 18:29:35 +0530

    Time: 5 ms (21.1%), Space: 60.7 MB (30.38%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -4,19 +4,10 @@
  */
 var moveZeroes = function (nums) {
     let i = 0;
-    let j = 1;
-    let n = nums.length;
-    while (j < n) {
-        if (nums[i] != 0) {
+    for (let j = 0; j < nums.length; j++) {
+        if (nums[j] != 0) {
+            [nums[i], nums[j]] = [nums[j], nums[i]];
             i++;
-            j++;
-        } else {
-            while (j < n && nums[j] != 0) {
-                [nums[i], nums[j]] = [nums[j], nums[i]];
-                i++;
-                j++;
-            }
-            j++;
         }
     }
 };
